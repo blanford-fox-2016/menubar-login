716c66ef8ba0eb3f8f67a25bd5897b97e04ab47c
Izhharuddin Syanmil <email": null>
2016-10-20 15:24:51 +0700

    login and logut validated

--- routes/index.js
+++ routes/index.js
@@ -5,11 +5,29 @@ var menu = model.Menu
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express', err: "" });
 });
 
 router.get('/register', function(req, res, next) {
   res.render('register', { title: 'Register Page' });
+});
+
+router.post('/login', function(req, res, next) {
+  var remme = req.body.remme;
+  var user = req.body.username;
+  var password = req.body.password;
+  menu.findOne({
+    where: {
+      username : user,
+      password : password
+    }
+  }).then((data, err) =>{
+    if (data !== null){
+      res.render('login', { title: 'Smile Factory', user });
+    } else {
+      res.render('index', { title: 'Express', err: "Password/username salah" });
+    }
+  })
 });
 
 router.post('/reg', function(req, res, next){
@@ -20,6 +38,7 @@ router.post('/reg', function(req, res, next){
     role : "normal"
   }
   menu.create(data)
+  res.render('index', { title: 'Express', err: "Selamat Anda Telah Terdaftar, Silahkan cek email anda atau kembali ke halaman login" });
 })
 
 module.exports = router;
