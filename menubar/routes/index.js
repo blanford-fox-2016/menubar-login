3caa772a6d7b91c6593a9aad053dbf119c8fee36
ahyanarizky <ahyana.rizky@gmail.com>
2016-10-20 15:01:13 +0700

    router login

--- menubar/routes/index.js
+++ menubar/routes/index.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var router = express.Router();
+var models = require('../models')
+var user = models.User
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -7,6 +9,46 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/reg', function(req, res, next) {
-  console.log(req.body);
+  if (req.body.username && req.body.email && req.body.password && req.body.role) {
+    user.findAll({
+      where:{email:req.body.email}
+    }).then(function(log){
+      if (log.length != 0) {
+        res.send('email sudah terpakai')// TODO:
+      }else{
+        user.create({
+          username: req.body.username,
+          email: req.body.email,
+          password: req.body.password,
+          role: req.body.role
+        }).then(function(user){
+          res.redirect('/')
+        })
+      }
+    })
+  } else {
+    res.send('Harus diisi semua')// TODO:
+    // kasih peringatan harus diisi
+  }
+})
+
+router.get('/login', function(req, res, next) {
+  res.render('login')
+})
+
+router.post('/login', function(req, res, next) {
+  if (req.body.username && req.body.password) {
+    user.findOne({
+      where:{username: req.body.username}
+    }).then(function(user) {
+      if (user.password == req.body.password) {
+        res.send(`Welcome ${user.username}`)
+      } else {
+        res.send('wrong password')// TODO:
+      }
+    })
+  } else {
+    res.send('email field must be filled')// TODO:
+  }
 })
 module.exports = router;
